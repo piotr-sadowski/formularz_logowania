7f46a6506a9abd9e9c636e2cc79ecc2832bfa3dd
piotr-sadowski <77448516+piotr-sadowski@users.noreply.github.com>
2022-11-27 22:10:46 +0100

    Added comments to html and js files

--- login.js
+++ login.js
@@ -1,15 +1,16 @@
+// creating 3 cons variable to have reference to elements which we will track
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+loginButton.addEventListener("click", (e) => {				// Event listener to handle what happen when Login button is click. e is a parameter for mouse click
+    e.preventDefault();										// preventing default action for click which is submit a data
+    const username = loginForm.username.value;				// store value inputed username field on form into username variable
+    const password = loginForm.password.value;				// store value inputed password field on form into password variable
 
-    if (username === "piotr" && password === "Test@123#") { // compare provided login name and password to hardcoded value
-        alert("You have successfully logged in.");
-        location.reload();
+    if (username === "piotr" && password === "Test@123#") { // if user provided correct username and password
+        alert("You have successfully logged in.");			// display alert that user logged in
+        location.reload();									// page is reload
     } else {
         loginErrorMsg.style.opacity = 1;
     }
